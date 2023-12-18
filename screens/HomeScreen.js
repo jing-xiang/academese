d21b78c8d6f5c0ae2b470ea6c9c8975d61a2c5f8
Chew Jing Xiang <chewjingxiangjumps@gmail.com>
2023-12-18 22:42:17 +0800

    initial commit

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,72 +1,107 @@
-// screens/HomeScreen.js
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import { Button, Card, Title, Paragraph } from 'react-native-paper';
-import LottieView from 'lottie-react-native'; // Import LottieView
+import {
+  View,
+  Text,
+  ImageBackground,
+  Dimensions,
+  FlatList,
+  TextInput,
+  SafeAreaView,
+} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import courses from '../const/courses.js';
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
-const HomeScreen = ({ navigation, onLogout }) => {
+
+const HomeScreen = ({navigation}) => {
+  const CourseCard = ({course}) => {
+    return (
+      <TouchableOpacity
+        activeOpacity={0.8}
+        onPress={() => navigation.navigate('CourseScreen', {data: course})}>
+        <ImageBackground
+          source={course.image}
+          style={{
+            marginVertical: 10,
+            marginHorizontal: 5,
+            borderRadius: 50,
+            width: windowWidth / 2 - 30,
+            height: windowHeight / 3,
+            paddingTop: 25,
+            paddingLeft: 20,
+            borderRadius: 15,
+            overflow: 'hidden',
+          }}>
+          <Text
+            style={{
+              fontSize: 20,
+              fontWeight: 'bold',
+              paddingBottom: 5,
+            }}>
+            {course.name}
+          </Text>
+          <Text style={{color: '#8F95B2', fontWeight: '600'}}>
+            {course.totalCourse + ' Courses'}
+          </Text>
+        </ImageBackground>
+      </TouchableOpacity>
+    );
+  };
   return (
-    <View style={styles.container}>
-      {/* Lottie Animation */}
-      <LottieView
-        source={require('../assets/animations/singing-contract.json')} 
-        autoPlay
-        loop
-        style={styles.animation}
-      />
-
-      {/* Card with Welcome Message */}
-      <Card style={styles.card}>
-        <Card.Content>
-          <Title style={styles.title}>Welcome to Academese!</Title>
-          <Paragraph style={styles.paragraph}>
-            Enhance your learning experience with Academese. Explore your schedule and profile to
-            make the most of your academic journey.
-          </Paragraph>
-        </Card.Content>
-      </Card>
-
-      {/* Buttons */}
-      <Button mode="contained" onPress={() => navigation.navigate('Profile')} style={styles.button}>
-        Go to Profile
-      </Button>
-      <Button mode="contained" onPress={() => navigation.navigate('Schedule')} style={styles.button}>
-        Go to Schedule
-      </Button>
-      <Button mode="contained" onPress={onLogout} color="#FF5733" style={styles.button}>
-        Logout
-      </Button>
-    </View>
+    <SafeAreaView
+      style={{
+        backgroundColor: '#fff',
+        flex: 1,
+        paddingHorizontal: 20,
+      }}>
+      <View style={{marginTop: 20}}>
+        <Text style={{fontSize: 28, fontWeight: 'bold'}}>Hey Alex,</Text>
+        <Text style={{fontSize: 22, color: '#61688B', marginTop: 15}}>
+          Find a course you want to learn
+        </Text>
+        <View
+          style={{
+            height: 60,
+            marginTop: 35,
+            paddingTop: 10,
+            paddingBottom: 10,
+            paddingLeft: 15,
+            backgroundColor: '#F5F5F7',
+            borderRadius: 30,
+            alignItems: 'center',
+            flexDirection: 'row',
+          }}>
+          <Icon size={30} name="search" />
+          <TextInput
+            style={{fontSize: 18, marginLeft: 5}}
+            placeholder="Search for anything"
+          />
+        </View>
+        <View
+          style={{
+            paddingVertical: 25,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+          }}>
+          <Text style={{fontSize: 20, fontWeight: 'bold'}}>Categories</Text>
+          <Text style={{fontSize: 18, fontWeight: 'bold', color: '#6E8AFA'}}>
+            See All
+          </Text>
+        </View>
+      </View>
+      <View style={{flex: 1}}>
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          numColumns={2}
+          data={courses}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({item}) => <CourseCard course={item} />}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 20,
-  },
-  animation: {
-    width: 200, // Adjust the width and height as needed
-    height: 200,
-    marginBottom: 20,
-  },
-  card: {
-    width: '100%',
-    marginBottom: 20,
-  },
-  title: {
-    fontSize: 24,
-    marginBottom: 10,
-    color: '#333', // Customize the color as needed
-  },
-  paragraph: {
-    fontSize: 16,
-  },
-  button: {
-    marginTop: 20,
-  },
-});
 
 export default HomeScreen;
