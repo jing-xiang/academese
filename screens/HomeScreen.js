18a609c09ab15250999d6db77599f632e29bf5d7
Chew Jing Xiang <chewjingxiangjumps@gmail.com>
2023-12-18 23:07:24 +0800

    initial commit

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,72 +1,143 @@
-// screens/HomeScreen.js
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import { Button, Card, Title, Paragraph } from 'react-native-paper';
-import LottieView from 'lottie-react-native'; // Import LottieView
+import React from 'react'
+import {View,Text,ImageBackground,TouchableOpacity,Image} from 'react-native'
+import {ScrollView,TextInput} from 'react-native-gesture-handler'
+import CourseList from '../screens/CourseList'
 
-const HomeScreen = ({ navigation, onLogout }) => {
-  return (
-    <View style={styles.container}>
-      {/* Lottie Animation */}
-      <LottieView
-        source={require('../assets/animations/singing-contract.json')} 
-        autoPlay
-        loop
-        style={styles.animation}
-      />
+export default class Home extends React.Component{
+    render(){
+        return(
+           <ImageBackground
+            source={require('../images/Home.png')}
+            style={{width:"100%",height:"100%"}}
+           >
+               <ScrollView>
+                   <View style={{
+                       width:"100%",
+                       alignItems:"flex-end",
+                       paddingHorizontal:20
+                   }}>
+                       <View style={{
+                           paddingHorizontal:10,
+                           paddingVertical:12,
+                           borderRadius:10,
+                           marginTop:30,
+                           backgroundColor:"#d1a0a7"
+                       }}>
+                           <Image
+                            source={require('../images/hum.png')}
+                            style={{height:15,width:20}}
+                           />
+                       </View>
+                   </View>
+                   <Text style={{
+                       paddingHorizontal:20,
+                       fontSize:35,
+                       paddingTop:40,
+                       fontFamily:"Bold",
+                       color:"#FFF"
+                   }}>
+                       Welcome back Mikolaj
+                   </Text>
 
-      {/* Card with Welcome Message */}
-      <Card style={styles.card}>
-        <Card.Content>
-          <Title style={styles.title}>Welcome to Academese!</Title>
-          <Paragraph style={styles.paragraph}>
-            Enhance your learning experience with Academese. Explore your schedule and profile to
-            make the most of your academic journey.
-          </Paragraph>
-        </Card.Content>
-      </Card>
+                   <View style={{
+                       flexDirection:"row",
+                       alignItems:"center",
+                       backgroundColor:"#FFF",
+                       padding:10,
+                       borderRadius:12,
+                       marginHorizontal:20,
+                       marginTop:20
+                   }}>
+                       <TextInput
+                            placeholder="Search for new knowledge!"
+                            placeholderTextColor="#345c74"
+                            style={{
+                                fontFamily:"Bold",
+                                fontSize:12,
+                                width:280,
+                                paddingHorizontal:12
+                            }}
+                       />
+                       <Image
+                            source={require('../images/sear.png')}
+                            style={{height:14,width:14}}
+                       />
+                   </View>
+                   <View style={{
+                       flexDirection:"row",
+                       backgroundColor:"#FFF2F2",
+                       marginTop:15,
+                       marginHorizontal:20,
+                       borderRadius:20,
+                       paddingVertical:30,
+                       paddingLeft:30
+                   }}>
+                       <View>
+                           <Text style={{
+                               color:"#345c74",
+                               fontSize:20,
+                               fontFamily:"Bold",
+                               width:250,
+                               paddingRight:100 
+                           }}>
+                               Start learning new Staff
+                           </Text>
+                           <TouchableOpacity
+                                onPress={()=>this.props.navigation.navigate('Cources')}
+                                style={{
+                                    flexDirection:"row",
+                                    backgroundColor:"#f58084",
+                                    alignItems:"center",
+                                    marginTop:20,
+                                    width:150,
+                                    paddingVertical:10,
+                                    borderRadius:14,
+                                    paddingHorizontal:10
+                                }}
+                           >
+                                    <Text style={{
+                                        color:"#FFF",
+                                        fontFamily:"Bold",
+                                        fontSize:12
+                                    }}>Categories</Text>  
+                                    <Image
+                                        source={require('../images/a3.png')}
+                                        style={{marginLeft:20,width:8,height:8}}
+                                    />
+                           </TouchableOpacity>
+                       </View>
+                       <Image
+                            source={require('../images/undraw.png')}
+                            style={{marginLeft:-80,marginTop:35}}
+                       />
 
-      {/* Buttons */}
-      <Button mode="contained" onPress={() => navigation.navigate('Profile')} style={styles.button}>
-        Go to Profile
-      </Button>
-      <Button mode="contained" onPress={() => navigation.navigate('Schedule')} style={styles.button}>
-        Go to Schedule
-      </Button>
-      <Button mode="contained" onPress={onLogout} color="#FF5733" style={styles.button}>
-        Logout
-      </Button>
-    </View>
-  );
-};
+                   </View>
+                   <Text style={{
+                       color:"#345c74",
+                       fontFamily:"Bold",
+                       fontSize:20,
+                       paddingHorizontal:20,
+                       marginTop:20,
+                       marginBottom:10
+                   }}>Courses in progress</Text>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 20,
-  },
-  animation: {
-    width: 200, // Adjust the width and height as needed
-    height: 200,
-    marginBottom: 20,
-  },
-  card: {
-    width: '100%',
-    marginBottom: 20,
-  },
-  title: {
-    fontSize: 24,
-    marginBottom: 10,
-    color: '#333', // Customize the color as needed
-  },
-  paragraph: {
-    fontSize: 16,
-  },
-  button: {
-    marginTop: 20,
-  },
-});
-
-export default HomeScreen;
+                   <CourseList
+                        img={require('../images/xd.png')}
+                        title="Adobe XD Prototyping"
+                        bg="#fdddf3"
+                   />
+                    <CourseList
+                        img={require('../images/sketch.png')}
+                        title="Sketch shortcuts and tricks"
+                        bg="#fef8e3"
+                   />
+                    <CourseList
+                        img={require('../images/ae.png')}
+                        title="UI Motion Design in After Effects"
+                        bg="#fcf2ff"
+                   />
+               </ScrollView>
+           </ImageBackground>
+        )
+    }
+}
